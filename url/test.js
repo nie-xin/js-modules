6060c4e44d706c689822ec4d09219755a4838592
nie xin <niexin.study@gmail.com>
2015-09-29 23:10:14 +0200

    complete tests

--- url/test.js
+++ url/test.js
@@ -7,4 +7,10 @@ test('url parse', function(assert) {
   var parser = url(testUrl);
 
   assert.equal(parser.protocol, 'http:');
+  assert.equal(parser.hostname, 'example.com');
+  assert.equal(parser.port, '3000');
+  assert.equal(parser.pathname, '/pathname/');
+  assert.equal(parser.search, '?search=test');
+  assert.equal(parser.hash, '#hash');
+  assert.equal(parser.host, 'example.com:3000');
 });
--- url/url.js
+++ url/url.js
@@ -1,17 +1,4 @@
-// var parser = document.createElement('a');
-// parser.href = "http://example.com:3000/pathname/?search=test#hash";
-//
-// parser.protocol; // => "http:"
-// parser.hostname; // => "example.com"
-// parser.port;     // => "3000"
-// parser.pathname; // => "/pathname/"
-// parser.search;   // => "?search=test"
-// parser.hash;     // => "#hash"
-// parser.host;     // => "example.com:3000"
-
-module.exports = Url;
-
-function Url(url) {
+module.exports = function (url) {
   var parser = document.createElement('a');
   parser.href = url;
 
